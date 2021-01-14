17ec85ba2f1528736902acc81b6bffea469e702d
mengshalitsofi <mengshalitsofi@gmail.com>
2021-01-14 12:55:45 -0800

    commit tree 741ba6ab943bd313b26b2fab175dd28d64c5a6a8

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(numbers)
+    numbers.sort do |a, b|
+        a <=> b
+    end
+end
 
+def sort_array_desc(numbers)
+    numbers.sort do |b, a|
+        a <=> b
+    end
+end
+ 
+def sort_array_char_count(strings)
+    strings.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements_from_to(array, index, destination_index)
+    temp = array[index]
+    array[index] = array[destination_index]
+    array[destination_index] = temp
+end
+
+def swap_elements(array)
+    swap_elements_from_to(array, 1, 2)
+    return array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |string|
+        if string.length > 2
+            string[2] = "$"
+        end
+    end
+end
+
+def find_a(array)
+    array.select do |string|
+        string.start_with?("a")
+    end
+end
+
+def sum_array(numbers)
+    numbers.inject { |sum, n| sum + n}
+end
+
+def add_s(array)
+    array.each_with_index.collect{|element, index| index == 1 ? element : (element + "s")}
+end
